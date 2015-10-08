54777d200d3be9de857c6b2ce5a8a652c935713f
bharath <bharathreddy_s@yahoo.com>
2015-10-08 17:16:06 +0530

    Initial Commit

---
+++ BRMultiController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRMultiController
+//
+//  Created by Bharath V on 08/10/15.
+//  Copyright © 2015 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
